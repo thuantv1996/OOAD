2421fd2bb827cebd913ce990dc8654b1d36e7a4b
ThuanTV <tuvanthuan1996@gmail.com>
2018-10-26 10:50:35 +0700

    Thuan-TV
    Add DAO

---
+++ O4/ClinicManagement/DAO/Imp/BaseDAO.cs
@@ -0,0 +1,289 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.Core.Objects;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace DAO.Imp
+{
+    public class BaseDAO
+    {
+        /// <summary>
+        /// Insert một Object Entity xuống Database
+        /// </summary>
+        /// <typeparam name="T">Type of Object</typeparam>
+        /// <param name="Entity">Object entity</param>
+        /// <param name="db">Database</param>
+        /// <param name="MessageError">List Error</param>
+        /// <returns>Result Execute</returns>
+        public string Insert<T>(T Entity, DbContext db, out List<string> MessageError) where T : class
+        {
+            MessageError = null;
+            try
+            {
+                // Lấy table T từ Database
+                DbSet<T> Table = db.Set<T>();
+                // Thêm Object vào Table
+                Table.Add(Entity);
+                // Lưu xữ lý
+                db.SaveChanges();
+                // Return success
+                return DAO.Com.DAOConstant.RES_SUC;
+            }
+            catch (Exception e)
+            {
+                // Tạo danh sách MessageError
+                MessageError = new List<string>();
+                // Thêm thông tin lỗi
+                MessageError.Add(DAO.Com.DAOUtils.GetErrorFromException(e));
+                // Return fail
+                return DAO.Com.DAOConstant.RES_FAI;
+            }
+
+        }
+
+        /// <summary>
+        /// Upfate một Object Entity
+        /// </summary>
+        /// <typeparam name="T">Type of Object</typeparam>
+        /// <param name="Entity">Object entity</param>
+        /// <param name="db">Database</param>
+        /// <param name="MessageError">List Error</param>
+        /// <returns>Result Execute</returns>
+        public string Update<T>(T Entity, DbContext db, out List<string> MessageError) where T : class
+        {
+            MessageError = null;
+            try
+            {
+                // Lấy Table T từ Database
+                DbSet<T> Table = db.Set<T>();
+                // Tìm kiếm đối tượng Entity old
+                // Hàm GetKeys trả về mảng Object các Key value của Entity
+                T UpdateObject = Table.Find(GetKeys<T>(Entity, db));
+                // Nếu Không tìm thấy đối tượng
+                if (UpdateObject == null)
+                {
+                    // Tạo mới MessageError
+                    MessageError = new List<string>();
+                    // Thông báo Mã lỗi
+                    MessageError.Add(DAO.Com.DAOMessage.MES_DAO_001);
+                    // Return fail
+                    return DAO.Com.DAOConstant.RES_FAI;
+                }
+                // Nếu tìm thấy đối tượng
+                // Sao chép giá trị các thuộc tính
+                db.Entry(UpdateObject).CurrentValues.SetValues(Entity);
+                // Lưu xữ lý
+                db.SaveChanges();
+                // Return success
+                return DAO.Com.DAOConstant.RES_SUC;
+            }
+            catch (Exception e)
+            {
+                // Tạo danh sách MessageError
+                MessageError = new List<string>();
+                // Thêm thông tin lỗi
+                MessageError.Add(DAO.Com.DAOUtils.GetErrorFromException(e));
+                // Return fail
+                return DAO.Com.DAOConstant.RES_FAI;
+            }
+        }
+
+        /// <summary>
+        /// Delete một Object Entity trong Database
+        /// </summary>
+        /// <typeparam name="T">Type of Object</typeparam>
+        /// <param name="Entity">Object entity</param>
+        /// <param name="db">Database</param>
+        /// <param name="MessageError">List Error</param>
+        /// <returns>Result Execute</returns>
+        public string Delete<T>(T Entity, DbContext db, out List<string> MessageError) where T : class
+        {
+            MessageError = null;
+            try
+            {
+                // Lấy Table T từ Database
+                DbSet<T> Table = db.Set<T>();
+                // Tìm kiếm đối tượng Entity old
+                // Hàm GetKeys trả về mảng Object các Key value của Entity
+                T DeleteObject = Table.Find(GetKeys<T>(Entity, db));
+                // Nếu Không tìm thấy đối tượng
+                if (DeleteObject == null)
+                {
+                    // Tạo mới MessageError
+                    MessageError = new List<string>();
+                    // Thông báo Mã lỗi
+                    MessageError.Add(DAO.Com.DAOMessage.MES_DAO_001);
+                    // Return fail
+                    return DAO.Com.DAOConstant.RES_FAI;
+                }
+                // Nếu tìm thấy đối tượng
+                // Xóa đối tượng đó trong Table
+                Table.Remove(DeleteObject);
+                // Lưu xữ lý
+                db.SaveChanges();
+                // Return success
+                return DAO.Com.DAOConstant.RES_SUC;
+            }
+            catch (Exception e)
+            {
+                // Tạo danh sách MessageError
+                MessageError = new List<string>();
+                // Thêm thông tin lỗi
+                MessageError.Add(DAO.Com.DAOUtils.GetErrorFromException(e));
+                // Return fail
+                return DAO.Com.DAOConstant.RES_FAI;
+            }
+        }
+
+        /// <summary>
+        /// Select một danh sách Object từ Database
+        /// </summary>
+        /// <typeparam name="T">Data type</typeparam>
+        /// <param name="db">Database</param>
+        /// <param name="Result">List Result</param>
+        /// <param name="MessageError">List Error</param>
+        /// <returns>Result Execute</returns>
+        public string Select<T>(DbContext db, out List<T> Result, out List<string> MessageError) where T : class
+        {
+            MessageError = null;
+            try
+            {
+                // Lấy Table T từ Database
+                DbSet<T> Table = db.Set<T>();
+                // Tạo mới danh sách kết quả trả về
+                Result = new List<T>();
+                // Lấy tất cả các record trong Table
+                Result = Table.ToList<T>();
+                // Return success
+                return DAO.Com.DAOConstant.RES_SUC;
+            }
+            catch (Exception e)
+            {
+                // Đặt kết quả trả về là null
+                Result = null;
+                // Tạo danh sách MessageError
+                MessageError = new List<string>();
+                // Thêm thông tin lỗi
+                MessageError.Add(DAO.Com.DAOUtils.GetErrorFromException(e));
+                // Return fail
+                return DAO.Com.DAOConstant.RES_FAI;
+            }
+        }
+
+        /// <summary>
+        /// Select một danh sách Object từ Database với mã Linq Expression
+        /// </summary>
+        /// <typeparam name="T">Data type</typeparam>
+        /// <param name="db">Database</param>
+        /// <param name="LinqSelector">Linq Expression</param>
+        /// <param name="Result">List Result</param>
+        /// <param name="MessageError">List Error</param>
+        /// <returns>Result Execute</returns>
+        public string Select<T>(DbContext db, Expression<Func<T, bool>> LinqSelector,
+                                out List<T> Result, out List<string> MessageError) where T : class
+        {
+            MessageError = null;
+
+            try
+            {
+                // Lấy Table T từ Database
+                DbSet<T> Table = db.Set<T>();
+                // Tạo mới danh sách kết quả trả về
+                Result = new List<T>();
+                // Lấy tất cả các record trong Table với d9eiu62 kiện search là LinqSelector
+                Result = Table.Where(LinqSelector).ToList<T>();
+                // Return success
+                return DAO.Com.DAOConstant.RES_SUC;
+            }
+            catch (Exception e)
+            {
+                // Đặt kết quả trả về là null
+                Result = null;
+                // Tạo danh sách MessageError
+                MessageError = new List<string>();
+                // Thêm thông tin lỗi
+                MessageError.Add(DAO.Com.DAOUtils.GetErrorFromException(e));
+                // Return fail
+                return DAO.Com.DAOConstant.RES_FAI;
+            }
+        }
+
+        /// <summary>
+        /// Select một danh sách Object từ Database với mã Linq Expression
+        /// </summary>
+        /// <typeparam name="T">Data type</typeparam>
+        /// <param name="db">Database</param>
+        /// <param name="LinqSelector">Linq Expression</param>
+        /// <param name="Result">List Result</param>
+        /// <param name="MessageError">List Error</param>
+        /// <returns>Result Execute</returns>
+        public string Select<T>(DbContext db, string sql, object[] param,
+                                out List<T> Result, out List<string> MessageError) where T : class
+        {
+            MessageError = null;
+
+            try
+            {
+                // Lấy Table T từ Database
+                DbSet<T> Table = db.Set<T>();
+                // Tạo mới danh sách kết quả trả về
+                Result = new List<T>();
+                // Lấy tất cả các record trong Table với điều kiện search là LinqSelector
+                Result = db.Database.SqlQuery<T>(sql,param).ToList<T>();
+                // Return success
+                return DAO.Com.DAOConstant.RES_SUC;
+            }
+            catch (Exception e)
+            {
+                // Đặt kết quả trả về là null
+                Result = null;
+                // Tạo danh sách MessageError
+                MessageError = new List<string>();
+                // Thêm thông tin lỗi
+                MessageError.Add(DAO.Com.DAOUtils.GetErrorFromException(e));
+                // Return fail
+                return DAO.Com.DAOConstant.RES_FAI;
+            }
+        }
+
+        /// <summary>
+        /// Phương thức lấy danh sách key của 1 table
+        /// copy https://michaelmairegger.wordpress.com/2013/03/30/find-primary-keys-from-entities-from-dbcontext/
+        /// </summary>
+        /// <typeparam name="T">Loại table</typeparam>
+        /// <param name="context">Database</param>
+        /// <returns>Danh sách tên các key của table T</returns>
+        private string[] GetKeyNames<T>(DbContext context) where T : class
+        {
+            ObjectSet<T> objectSet = ((IObjectContextAdapter)context).ObjectContext.CreateObjectSet<T>();
+            string[] keyNames = objectSet.EntitySet.ElementType.KeyMembers
+                                                               .Select(k => k.Name)
+                                                               .ToArray();
+            return keyNames;
+        }
+
+        /// <summary>
+        /// Phương thức lấy ra danh sách key value
+        /// copy https://michaelmairegger.wordpress.com/2013/03/30/find-primary-keys-from-entities-from-dbcontext/
+        /// </summary>
+        /// <typeparam name="T">Table type</typeparam>
+        /// <param name="entity">entity</param>
+        /// <param name="context">database</param>
+        /// <returns>Danh sách các key value</returns>
+        private object[] GetKeys<T>(T entity, DbContext context) where T : class
+        {
+            var keyNames = GetKeyNames<T>(context);
+            Type type = typeof(T);
+
+            object[] keys = new object[keyNames.Length];
+            for (int i = 0; i < keyNames.Length; i++)
+            {
+                keys[i] = type.GetProperty(keyNames[i]).GetValue(entity, null);
+            }
+            return keys;
+        }
+    }
+}
--- O4/ClinicManagement/DAO/MainTest.cs
+++ O4/ClinicManagement/DAO/MainTest.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using DAO.Imp;
+using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,29 +12,80 @@ namespace DAO
     {
         public static void Main(string[] args)
         {
-            List<My> res = null;
-            List<string> mes = null;
-            DAOAdvance<My>.SqlSelExe(get,out res, mes);
+            // Hướng dẫn sử dụng DAO
 
-            foreach(var i in res)
+            /* Dùng DAO select Dữ liệu từ một bảng*/
+            using (QLPHONGKHAMEntities db = new QLPHONGKHAMEntities())
             {
-                My l = i as My;
-                System.Console.Write(l.Loai);
-            }
-        }
+                // Khai báo DAO
+                BaseDAO dao = new BaseDAO();
+                // Danh sách kết quả trả về
+                List<LOAIHOSO> LstRest = null;
+                // Danh sách MessageError
+                List<string> MessageError = null;
+                // return Id
+                string IdResult = "";
+                // Ví dụ lấy tất cả data bảng LOAIHOSO
+                IdResult = dao.Select(db, out LstRest, out MessageError);
+                // Hoặc để minh bạch hơn có thể viết lệnh như sau
+                IdResult = dao.Select<LOAIHOSO>(db, out LstRest, out MessageError);
+                // hai lệnh trên gọi đến cùng một function
 
-        private static List<My> get()
-        {
-            using (var db = new QLPHONGKHAMEntities())
-            {
-                List<My> res = (from l in db.LOAIHOSOes
-                                      select new My { Loai = l.TenLoaiHoSo }).ToList();
-                return res;
+                // Để Select có điều kiện bắt buộc các bạn cần phải biết Linq
+                // Ví dụ select tất cả các bảng ghi có mã Loại hồ sơ là LHS0000001
+                IdResult = dao.Select(db, lhs => lhs.MaLoaiHoSo == "LHS0000001", out LstRest, out MessageError);
+                // Chú ý trong ví dụ trên tham số thứ 2 là Lamda function các bạn cần tìm hiểu cái này
+                // Cái này khá quen thuộc và cần thiết khi các bạn làm việc với JS react native
+                // Các bạn có thể hiểu nôm na là các phần tử trước '=>' là tham số truyền vào hàm lambda
+                // Các mã viết sau '=>' là biểu thức thực hiện hàm lambda đó
+
+                // Để thực hiện các câu Select phức tạp hơn cần viết SQL
+                // ví dụ thực hiện Lấy Ra danh sách Nhân viên có Mã Loại Nhân Viên LNV0000001
+                string sql = "Select NHANVIEN.HoTenNV AS TenNhanVien,LOAINHANVIEN.TenLoaiNV AS TenLoaiNhanVien " +
+                             "from NHANVIEN,LOAINHANVIEN " +
+                             "WHERE NHANVIEN.MaLoaiNV = LOAINHANVIEN.MaLoaiNV and LOAINHANVIEN.MaLoaiNV={0}";
+                List<ExtendEmployee> LstRestEmp = null;
+                object[] param = { "LNV0000001" };
+                dao.Select<ExtendEmployee>(db, sql, param, out LstRestEmp, out MessageError);
             }
-        }
+
+            /* Dùng DAO insert Dữ liệu vào một bảng*/
+            using (QLPHONGKHAMEntities db = new QLPHONGKHAMEntities())
+            {
+                // Transaction quản lý việc thực thi chuỗi nghiệp vụ
+                using (var transaction = db.Database.BeginTransaction())
+                {
+                    // Khai báo DAO
+                    BaseDAO dao = new BaseDAO();
+                    // Danh sách kết quả trả về
+                    List<LOAIHOSO> LstRest = null;
+                    // Danh sách MessageError
+                    List<string> MessageError = null;
+                    // return Id
+                    string IdResult = "";
+                    // Entity
+                    LOAIHOSO entity = new LOAIHOSO { MaLoaiHoSo = "LHS0000001", TenLoaiHoSo = "Hồ sơ khám mới" };
+                    // thực thi insert 
+                    IdResult = dao.Insert(entity, db, out MessageError);
+                    
+                    if(IdResult == DAO.Com.DAOConstant.RES_SUC)
+                    {
+                        // Cần phải commit data sau khi thực thi chuỗi nghiệp vụ
+                        transaction.Commit();
+                    }
+                    else
+                    {
+                        // nếu có bất cứ thực thi nào error thì phải rollback data
+                        transaction.Rollback();
+                    }
+                }
+            }
+            // Các lệnh Update, Delete sử dụng tương tự lệnh insert
     }
-    class My
+    class ExtendEmployee
     {
-        public string Loai;
+            public string TenNhanVien { get; set; }
+            public String TenLoaiNhanVien { get; set; }
     }
+
 }
