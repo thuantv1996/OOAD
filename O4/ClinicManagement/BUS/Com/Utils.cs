8112c9f2522cd9eed42fda28a04f2bf4d626e035
sangyn0705 <32682729+sanguyn@users.noreply.github.com>
2018-10-30 11:12:48 +0700

    nguyen sa

--- O4/ClinicManagement/BUS/Com/Utils.cs
+++ O4/ClinicManagement/BUS/Com/Utils.cs
@@ -14,7 +14,6 @@ namespace BUS.Com
         {
             var fromProperties = parent.GetType().GetProperties();
             var toProperties = self.GetType().GetProperties();
-
             foreach (var fromProperty in fromProperties)
             {
                 foreach (var toProperty in toProperties)
@@ -27,10 +26,29 @@ namespace BUS.Com
                             break;
                         }
                         catch { }
-                        
+
                     }
                 }
             }
         }
+
+        public string CreateMD5(string input)
+        {
+            // Use input string to calculate MD5 hash
+            using (System.Security.Cryptography.MD5 md5 = System.Security.Cryptography.MD5.Create())
+            {
+                byte[] inputBytes = System.Text.Encoding.ASCII.GetBytes(input);
+                byte[] hashBytes = md5.ComputeHash(inputBytes);
+
+                // Convert the byte array to hexadecimal string
+                StringBuilder sb = new StringBuilder();
+                for (int i = 0; i < hashBytes.Length; i++)
+                {
+                    sb.Append(hashBytes[i].ToString("X2"));
+                }
+                return sb.ToString();
+            }
+        }
     }
+
 }
