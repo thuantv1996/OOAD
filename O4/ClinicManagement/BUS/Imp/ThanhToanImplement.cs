706935783cfa3f7f214dcd521d3dafc6940d89da
pham truong tieu phung <phungptt1309@gmail.com>
2018-11-04 14:02:47 +0700

    thanhtoan

---
+++ O4/ClinicManagement/BUS/Imp/ThanhToanImplement.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BUS.Entities;
+using BUS.Service;
+using DTO;
+using DAO;
+using COM;
+
+namespace BUS.Imp
+{
+    class ThanhToanImplement : IThanhToanService
+    {
+        public string AddThanhToan(QLPHONGKHAMEntities db, ThanhToanEntity ThanhToan, ref List<MessageError> Messages)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string UpdateThanhToan(QLPHONGKHAMEntities db, ThanhToanEntity ThanhToan, ref List<MessageError> Messages)
+        {
+            string ProgramName = "ThanhToanImplement-Update";
+            // khai báo các biến đón kết quả rả về từ Select
+            List<THANHTOAN> ListSelectResult = null;
+            string IdResult;
+            THANHTOAN ThanhToanDAO = new THANHTOAN();
+            BUS.Com.Utils.CopyPropertiesFrom(ThanhToan, ThanhToanDAO);
+
+
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ O4/ClinicManagement/DTO/ThanhToanEntity.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO
+{
+    public class ThanhToanEntity
+    {
+        public string MaThanhToan { get; set; }
+        public string MaHoSo { get; set; }
+        public Nullable<decimal> ChiPhiKham { get; set; }
+        public Nullable<decimal> ChiPhiXetNghiem { get; set; }
+        public Nullable<decimal> TongChiPhi { get; set; }
+        public string NhanVienThu { get; set; }
+        public string NgayThu { get; set; }
+
+        
+    }
+}
