0efcac6201a629303357adb977e679ff891a8559
pham truong tieu phung <phungptt1309@gmail.com>
2018-11-04 13:55:22 +0700

    Update BUS_BenhNhan_PhongKham_TrangThaiPhong

---
+++ O4/ClinicManagement/BUS/Service/ITrangThaiPhongService.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using DTO;
+using COM;
+using BUS.Entities;
+
+namespace BUS.Service
+{
+    interface ITrangThaiPhongService
+    {
+        /// <summary>
+        /// LẤY THÔNG TIN TRẠNG THÁI PHÒNG
+        /// </summary>
+        /// <param name="MaPhongKham"></param>
+        /// <param name="NgayThang"></param>
+        /// <param name="TrangThaiPhong"></param>
+        /// <param name="Messages"></param>
+        /// <returns></returns>
+        string GetTrangThaiPhong(string MaPhongKham, string NgayThang, out TrangThaiPhongEnity TrangThaiPhong, ref List<MessageError> Messages);
+        /// <summary>
+        /// UPDATE TRẠNG THÁI PHÒNG
+        /// </summary>
+        /// <param name="TrangThaiPhong"></param>
+        /// <param name="Messages"></param>
+        /// <returns></returns>
+        string UpdateTrangThaiPhong(TrangThaiPhongEnity TrangThaiPhong, ref List<MessageError> Messages);
+    }
+}
