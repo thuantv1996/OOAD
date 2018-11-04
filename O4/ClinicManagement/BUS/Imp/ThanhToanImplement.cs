83ef5c50f983398dcd32d51689024a401f6bb2b3
ThuanTV <tuvanthuan1996@gmail.com>
2018-11-04 15:08:35 +0700

    Thuan-TV review source cua Sa

--- O4/ClinicManagement/BUS/Imp/ThanhToanImplement.cs
+++ O4/ClinicManagement/BUS/Imp/ThanhToanImplement.cs
@@ -18,89 +18,56 @@ namespace BUS.Imp
             string ProgramName = "ThanhToanImplement-AddThanhToan";
             string IdResult;
             // Tạo đối tượng THANHTOAN kết quả
-            THANHTOAN ThanhToanResult = new THANHTOAN();
-            
+            THANHTOAN ThanhToanDAO = new THANHTOAN();
+
             // Convert đối tượng từ DTO sang DAO
-            BUS.Com.Utils.CopyPropertiesFrom(ThanhToan, ThanhToanResult);
-            // Khởi tạo Database
-            using (db = new QLPHONGKHAMEntities())
+            BUS.Com.Utils.CopyPropertiesFrom(ThanhToan, ThanhToanDAO);
+
+            // Khởi tạo lớp DAO
+            DAO.Imp.BaseDAO Dao = new DAO.Imp.BaseDAO();
+            // Thực hiện lệnh INSERT
+            IdResult = Dao.Insert(ThanhToanDAO, db, ref Messages);
+            // Nếu hàm INSERT báo lỗi
+            if (IdResult == Constant.RES_FAI)
             {
-                // Khởi tạo transaction 
-                using (var trans = db.Database.BeginTransaction())
+                // Thêm thông báo lỗi
+                Messages.Add(new MessageError
                 {
-                    // Khởi tạo lớp DAO
-                    DAO.Imp.BaseDAO Dao = new DAO.Imp.BaseDAO();
-                    // Thực hiện lệnh INSERT
-                    IdResult = Dao.Insert(ThanhToanResult ,db, ref Messages);
-                    // Nếu hàm INSERT báo lỗi
-                    if (IdResult == Constant.RES_FAI)
-                    {
-                        // Thêm thông báo lỗi
-                        Messages.Add(new MessageError
-                        {
-                            IdError = Constant.MES_DB,
-                            Message = string.Format("Lỗi khi Update vao Table THANHTOAN - {0}", ProgramName)
-                        });
-                        // Rollback dữ liệu
-                        trans.Rollback();
-                        // Return faild
-                        IdResult = Constant.RES_FAI;
-                        // return 
-                        return IdResult;
-                    }
-                    else
-                    {
-                        trans.Commit();
-                    }
-                }
+                    IdError = Constant.MES_DB,
+                    Message = string.Format("Lỗi khi Insert vao Table THANHTOAN - {0}", ProgramName)
+                });
+                // Return faild
+                return Constant.RES_FAI;
             }
             return Constant.RES_SUC;
         }
-
 
         public string UpdateThanhToan(QLPHONGKHAMEntities db, ThanhToanEntity ThanhToan, ref List<MessageError> Messages)
         {
             string ProgramName = "ThanhToanImplement-UpdateThanhToan";
             string IdResult;
             // Tạo đối tượng THANHTOAN kết quả
-            THANHTOAN ThanhToanResult = new THANHTOAN();
+            THANHTOAN ThanhToanDAO = new THANHTOAN();
             // Convert đối tượng từ DTO sang DAO
-            BUS.Com.Utils.CopyPropertiesFrom(ThanhToan, ThanhToanResult);
-            // Khởi tạo Database
-            using (db = new QLPHONGKHAMEntities())
+            BUS.Com.Utils.CopyPropertiesFrom(ThanhToan, ThanhToanDAO);
+            // Khởi tạo lớp DAO
+            DAO.Imp.BaseDAO Dao = new DAO.Imp.BaseDAO();
+            // Thực hiện lệnh Update
+            IdResult = Dao.Update(ThanhToanDAO, db, ref Messages);
+            // Nếu hàm INSERT báo lỗi
+            if (IdResult == Constant.RES_FAI)
             {
-                // Khởi tạo transaction 
-                using (var trans = db.Database.BeginTransaction())
+                // Thêm thông báo lỗi
+                Messages.Add(new MessageError
                 {
-                    // Khởi tạo lớp DAO
-                    DAO.Imp.BaseDAO Dao = new DAO.Imp.BaseDAO();
-                    // Thực hiện lệnh Update
-                    IdResult = Dao.Update(ThanhToanResult, db, ref Messages);
-                    // Nếu hàm INSERT báo lỗi
-                    if (IdResult == Constant.RES_FAI)
-                    {
-                        // Thêm thông báo lỗi
-                        Messages.Add(new MessageError
-                        {
-                            IdError = Constant.MES_DB,
-                            Message = string.Format("Lỗi khi Update vao Table THANHTOAN - {0}", ProgramName)
-                        });
-                        // Rollback dữ liệu
-                        trans.Rollback();
-                        // Return faild
-                        IdResult = Constant.RES_FAI;
-                        // return 
-                        return IdResult;
-                    }
-                    else
-                    {
-                        trans.Commit();
-                    }
-                }
+                    IdError = Constant.MES_DB,
+                    Message = string.Format("Lỗi khi Update vao Table THANHTOAN - {0}", ProgramName)
+                });
+                // Return faild
+                return Constant.RES_FAI;
             }
             return Constant.RES_SUC;
         }
-
     }
-            
+
 }
