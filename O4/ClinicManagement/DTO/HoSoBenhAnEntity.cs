9b4f34c89095bb7334c116074528c574182ff0e1
ThuanTV <tuvanthuan1996@gmail.com>
2018-11-09 10:11:46 +0700

    Thuan-TV review source

--- O4/ClinicManagement/DTO/HoSoBenhAnEntity.cs
+++ O4/ClinicManagement/DTO/HoSoBenhAnEntity.cs
@@ -13,9 +13,7 @@ namespace DTO
         public string MaHoSoGoc { get; set; }
         public string MaLoaiHoSo { get; set; }
         public string MaBenhNhan { get; set; }
-        public string HoTenBenhNhan { get; set; }
         public string MaNguoiTN { get; set; }
-        public string HoTenNguoiTN { get; set; }
         public string NgayTiepNhan { get; set; }
         public string YeuCauKham { get; set; }
         public string TrieuChung { get; set; }
@@ -23,7 +21,6 @@ namespace DTO
         public int SoThuTu { get; set; }
         public string MaPhongKham { get; set; }
         public string MaBacSi { get; set; }
-        public string HoTenBacSi { get; set; }
         public string ChuanDoan { get; set; }
         public bool CoKeDon { get; set; }
     }
