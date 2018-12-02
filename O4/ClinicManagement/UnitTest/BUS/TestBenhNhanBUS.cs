ef17f67e416b19c7b282adaa190492cbe061b483
Thuan <tuvanthuan1996@gmail.com>
2018-12-02 14:21:23 +0700

    Thuan-TV
    review surce Sa

--- O4/ClinicManagement/UnitTest/BUS/TestBenhNhanBUS.cs
+++ O4/ClinicManagement/UnitTest/BUS/TestBenhNhanBUS.cs
@@ -1,18 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
-using BUS;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using DTO;
 using System.Data.Entity;
-using System.Collections.Generic;
-using System.Linq;
-using Newtonsoft.Json;
-using DAO.Implement;
 using DAO;
 using BUS.Imp;
+using BUS.Entities;
+using System.Collections.Generic;
 
 namespace UnitTest.BUS
 {
@@ -26,12 +18,12 @@ namespace UnitTest.BUS
         public void Init()
         {
             db = new QLPHONGKHAMEntities();
-            trans = db.Database.BeginTransaction();
+            trans = db.Database.BeginTransaction(); 
         }
         [TestMethod]
         public void Insert_TestCase1()
         {
-            BenhNhanDTO benhNhanDTO = new BenhNhanDTO  { MaBenhNhan = "1234567890",
+            BenhNhanDTO benhNhanDTO = new BenhNhanDTO  { MaBenhNhan = "1234567899",
                                                          HoTen = "Nguyen Van A",
                                                          CMND ="2000900",
                                                          NgaySinh ="18091997",
@@ -44,19 +36,19 @@ namespace UnitTest.BUS
             string expected = "0000";
             Assert.Equals(actual, expected);
         }
+        [TestMethod]
         public void Insert_TestCase2()
         {
-            BenhNhanDTO benhNhanDTO = new BenhNhanDTO  { MaBenhNhan = "1234567890000"  };
+            BenhNhanDTO benhNhanDTO = new BenhNhanDTO  { MaBenhNhan = "1234567899"  };
             BenhNhanBUS benhNhanBUS = new BenhNhanBUS();
             string actual = benhNhanBUS.InsertBenhNhan(db, benhNhanDTO);
             string expected = "1111";
             Assert.Equals(actual, expected);
-
         }
-
+        [TestMethod]
         public void Insert_TestCase3()
         {
-            BenhNhanDTO benhNhanDTO = new BenhNhanDTO { MaBenhNhan = "1234567890000",
+            BenhNhanDTO benhNhanDTO = new BenhNhanDTO { MaBenhNhan = "1234567899",
                                                         HoTen = "12345678901234567890123456789012345678901234567890",
                                                         CMND ="2000900000000",
                                                         NgaySinh ="180919979",
@@ -70,166 +62,138 @@ namespace UnitTest.BUS
             string expected = "1111";
             Assert.Equals(actual, expected);
         }
-
+        [TestMethod]
         public void Search_TestCase1()
         {
-            BenhNhanDTO benhNhanDTO = new BenhNhanDTO
+            // create data search
+            db.BENHNHANs.Add(new BENHNHAN { MaBenhNhan = "1234567890", HoTen = "aaaaaaaaaa" });
+            db.BENHNHANs.Add(new BENHNHAN { MaBenhNhan = "1234567891", HoTen = "bbbbbbbbbb" });
+            db.BENHNHANs.Add(new BENHNHAN { MaBenhNhan = "1234567892", HoTen = "cccccccccc" });
+            // create object search
+            BenhNhanSearchEntity benhNhanSearch = new BenhNhanSearchEntity
             {
-                MaBenhNhan = "1234567890",
-                HoTen = "Nguyen Van A",
-                CMND = "2000900"
+                MaBenhNhan = "12345678",
+                TenBenhNhan = "",
+                CMND = ""
+            };
+            List<BenhNhanDTO> listResult = null;
+            BenhNhanBUS benhNhanBUS = new BenhNhanBUS();
+            // execute
+            string actual = benhNhanBUS.SearchBenhNhan(db, benhNhanSearch, out listResult);
+            string expected = "0000";
+            // compare
+            Assert.Equals(actual, expected);
+        }
+        [TestMethod]
+        public void Search_TestCase2()
+        {
+            // create object search
+            BenhNhanSearchEntity benhNhanSearch = new BenhNhanSearchEntity
+            {
+                MaBenhNhan = "12345678",
+                TenBenhNhan = "",
+                CMND = ""
             };
             BenhNhanBUS benhNhanBUS = new BenhNhanBUS();
-            string actual = benhNhanBUS.InsertBenhNhan(db, benhNhanDTO);
+            List<BenhNhanDTO> listResult = null;
+            string actual = benhNhanBUS.SearchBenhNhan(db, benhNhanSearch, out listResult);
             string expected = "0000";
             Assert.Equals(actual, expected);
         }
-
-        public void Search_TestCase2()
-        {
-            BenhNhanDTO benhNhanDTO = new BenhNhanDTO  {MaBenhNhan = "1234567890000",
-                                                        HoTen = "12345678901234567890123456789012345678901234567890",
-                                                        CMND ="2000900000000"};
-            BenhNhanBUS benhNhanBUS = new BenhNhanBUS();
-            string actual = benhNhanBUS.InsertBenhNhan(db, benhNhanDTO);
-            string expected = "1111";
-            Assert.Equals(actual, expected);
-        }
-
-         public void Update_TestCase1()
+        [TestMethod]
+        public void Update_TestCase1()
         {
             BenhNhanBUS benhNhanBUS = new BenhNhanBUS();
             // Insert benh nhan mới
-           BenhNhanDTO benhNhanDTO = new BenhNhanDTO { MaBenhNhan = "1234567899",
+            BenhNhanDTO benhNhanDTO = new BenhNhanDTO { MaBenhNhan = "1234567899",
                                                        HoTen = "Nguyen Van B" };
 
             benhNhanBUS.InsertBenhNhan(db, benhNhanDTO);
             // Update benh nhan
             BenhNhanDTO benhNhanUpdate = new BenhNhanDTO { MaBenhNhan = "1234567899",
-                                                           HoTen = "Nguyen Van B"};
+                                                           HoTen = "Nguyen Van A"};
 
 
-            string actual = benhNhanBUS.InsertBenhNhan(db, benhNhanUpdate);
+            string actual = benhNhanBUS.UpdateBenhNhan(db, benhNhanUpdate);
             string expected = "0000";
             // Test 
             Assert.Equals(expected, actual);
         }
-         public void Update_TestCase2()
+        [TestMethod]
+        public void Update_TestCase2()
         {
             BenhNhanBUS benhNhanBUS = new BenhNhanBUS();
             // Insert benh nhan mới
-           BenhNhanDTO benhNhanDTO = new BenhNhanDTO { MaBenhNhan = "",
-                                                       HoTen = "" };
-
+            BenhNhanDTO benhNhanDTO = new BenhNhanDTO { MaBenhNhan = "1234567899",
+                                                       HoTen = "Nguyen Van A" };
             benhNhanBUS.InsertBenhNhan(db, benhNhanDTO);
             // Update benh nhan
-            BenhNhanDTO benhNhanUpdate = new BenhNhanDTO { MaBenhNhan = "",
+            BenhNhanDTO benhNhanUpdate = new BenhNhanDTO { MaBenhNhan = "1234567899",
                                                            HoTen = ""};
-
-
-            string actual = benhNhanBUS.InsertBenhNhan(db, benhNhanUpdate);
+            string actual = benhNhanBUS.UpdateBenhNhan(db, benhNhanUpdate);
             string expected = "1111";
             // Test 
             Assert.Equals(expected, actual);
         }
-
-         public void Update_TestCase3()
+        [TestMethod]
+        public void Update_TestCase3()
         {
             BenhNhanBUS benhNhanBUS = new BenhNhanBUS();
             // Insert benh nhan mới
-           BenhNhanDTO benhNhanDTO = new BenhNhanDTO { MaBenhNhan = "1234567890000",
-                                                        HoTen = "12345678901234567890123456789012345678901234567890" };
-
+            BenhNhanDTO benhNhanDTO = new BenhNhanDTO { MaBenhNhan = "1234567899",
+                                                        HoTen = "Nguyen Van A" };
             benhNhanBUS.InsertBenhNhan(db, benhNhanDTO);
             // Update benh nhan
-            BenhNhanDTO benhNhanUpdate = new BenhNhanDTO { MaBenhNhan = "1234567890000",
+            BenhNhanDTO benhNhanUpdate = new BenhNhanDTO { MaBenhNhan = "1234567899",
                                                         HoTen = "12345678901234567890123456789012345678901234567890"};
-
-
             string actual = benhNhanBUS.InsertBenhNhan(db, benhNhanUpdate);
             string expected = "1111";
             // Test 
             Assert.Equals(expected, actual);
         }
-
-
-
-
-
+        [TestMethod]
         public void GetInformation_TestCase1()
         {
-            BenhNhanDTO benhNhanDTO = new BenhNhanDTO { MaBenhNhan = "1234567890" };
+            // create data search
+            db.BENHNHANs.Add(new BENHNHAN { MaBenhNhan = "1234567890", HoTen = "aaaaaaaaaa" });
+            db.BENHNHANs.Add(new BENHNHAN { MaBenhNhan = "1234567891", HoTen = "bbbbbbbbbb" });
+            db.BENHNHANs.Add(new BENHNHAN { MaBenhNhan = "1234567892", HoTen = "cccccccccc" });
+            BenhNhanDTO benhNhanDTO = null;
             BenhNhanBUS benhNhanBUS = new BenhNhanBUS();
-            string actual = benhNhanBUS.InsertBenhNhan(db, benhNhanDTO);
+            string actual = benhNhanBUS.GetInformationBenhNhan(db, "1234567890", out benhNhanDTO);
             string expected = "0000";
             Assert.Equals(actual, expected);
-
         }
+        [TestMethod]
         public void GetInformation_TestCase2()
         {
-            BenhNhanDTO benhNhanDTO = new BenhNhanDTO { MaBenhNhan = "" };
+            BenhNhanDTO benhNhanDTO = null;
             BenhNhanBUS benhNhanBUS = new BenhNhanBUS();
-            string actual = benhNhanBUS.InsertBenhNhan(db, benhNhanDTO);
+            string actual = benhNhanBUS.GetInformationBenhNhan(db, "1234567890", out benhNhanDTO);
             string expected = "1111";
             Assert.Equals(actual, expected);
 
         }
-
-        public void GetInformation_TestCase3()
-        {
-            BenhNhanDTO benhNhanDTO = new BenhNhanDTO { MaBenhNhan = "1234567890000" };
-            BenhNhanBUS benhNhanBUS = new BenhNhanBUS();
-            string actual = benhNhanBUS.InsertBenhNhan(db, benhNhanDTO);
-            string expected = "1111";
-            Assert.Equals(actual, expected);
-
-        }
-
+        [TestMethod]
         public void GetList_TestCase1()
         {
-            BenhNhanDTO benhNhanDTO = new BenhNhanDTO  { MaBenhNhan = "1234567890",
-                                                         HoTen = "Nguyen Van A",
-                                                         CMND ="2000900",
-                                                         NgaySinh ="18091997",
-                                                         GioiTinh = true,
-                                                         SoDienThoai="0913857555",
-                                                         DiaChi = "Ho Chi Minh",
-                                                         GhiChu = "Khong co"};
+            // create data search
+            db.BENHNHANs.Add(new BENHNHAN { MaBenhNhan = "1234567890", HoTen = "aaaaaaaaaa" });
+            db.BENHNHANs.Add(new BENHNHAN { MaBenhNhan = "1234567891", HoTen = "bbbbbbbbbb" });
+            db.BENHNHANs.Add(new BENHNHAN { MaBenhNhan = "1234567892", HoTen = "cccccccccc" });
+            List<BenhNhanDTO> listBenhNhan = null;
             BenhNhanBUS benhNhanBUS = new BenhNhanBUS();
-            string actual = benhNhanBUS.InsertBenhNhan(db, benhNhanDTO);
+            string actual = benhNhanBUS.GetListBenhNhan(db, out listBenhNhan);
             string expected = "0000";
             Assert.Equals(actual, expected);
 
         }
+        [TestMethod]
         public void GetList_TestCase2()
         {
-            BenhNhanDTO benhNhanDTO = new BenhNhanDTO { MaBenhNhan = "1234567890000",
-                                                        HoTen = "",
-                                                         CMND ="",
-                                                         NgaySinh ="",
-                                                         GioiTinh = true,
-                                                         SoDienThoai="",
-                                                         DiaChi = "",
-                                                         GhiChu = ""};
+            List<BenhNhanDTO> listBenhNhan = null;
             BenhNhanBUS benhNhanBUS = new BenhNhanBUS();
-            string actual = benhNhanBUS.InsertBenhNhan(db, benhNhanDTO);
-            string expected = "1111";
-            Assert.Equals(actual, expected);
-
-        }
-        public void GetList_TestCase3()
-        {
-            BenhNhanDTO benhNhanDTO = new BenhNhanDTO { MaBenhNhan = "1234567890000",
-                                                        HoTen = "12345678901234567890123456789012345678901234567890",
-                                                        CMND ="2000900000000",
-                                                        NgaySinh ="180919979",
-                                                        GioiTinh = true,
-                                                        SoDienThoai="091385755555",
-                                                        DiaChi = "1234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890",
-                                                        GhiChu = "1234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890"
-            };
-            BenhNhanBUS benhNhanBUS = new BenhNhanBUS();
-            string actual = benhNhanBUS.InsertBenhNhan(db, benhNhanDTO);
+            string actual = benhNhanBUS.GetListBenhNhan(db, out listBenhNhan);
             string expected = "1111";
             Assert.Equals(actual, expected);
 
