65deacf42cd3461b11183a57efad0b13e526c5a8
thuantv1996 <tuvanthuan1996@gmail.com>
2019-01-05 13:15:13 +0700

    Thuan-TV

---
+++ O4/ClinicManagement/ClinicManagement/Report/Windows/ThanhToanReport.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ClinicManagement.Report.Windows
+{
+    public partial class ThanhToanReport : Form
+    {
+        private Microsoft.Reporting.WinForms.ReportViewer reportViewer1;
+
+        public ThanhToanReport()
+        {
+            InitializeComponent();
+        }
+
+        private void InitializeComponent()
+        {
+            this.reportViewer1 = new Microsoft.Reporting.WinForms.ReportViewer();
+            this.SuspendLayout();
+            // 
+            // reportViewer1
+            // 
+            this.reportViewer1.Location = new System.Drawing.Point(0, 0);
+            this.reportViewer1.Name = "ReportViewer";
+            this.reportViewer1.Size = new System.Drawing.Size(396, 246);
+            this.reportViewer1.TabIndex = 0;
+            // 
+            // ThanhToanReport
+            // 
+            this.ClientSize = new System.Drawing.Size(739, 337);
+            this.Name = "ThanhToanReport";
+            this.ResumeLayout(false);
+
+        }
+    }
+}
